29b4834f95316e1ec0099dca025b10ef94ce4f5e
raselroky <raselroky016@gmail.com>
2024-05-26 12:01:15 +0600

    crud and data searc use binary search

--- crud/models.py
+++ crud/models.py
@@ -8,6 +8,26 @@ class News(models.Model):
     image=models.ImageField(upload_to='images/',null=True,blank=True)
     date=models.DateTimeField(auto_now_add=True)
     
+    # @classmethod
+    # def binary_search(cls, target_value):
+    #     # Retrieve all objects sorted by field_name
+    #     queryset = cls.objects.order_by('field_name')
+        
+    #     low = 0
+    #     high = queryset.count() - 1
+
+    #     while low <= high:
+    #         mid = (low + high) // 2
+    #         mid_value = queryset[mid].field_name
+    #         if mid_value == target_value:
+    #             return queryset[mid]  # Found the record
+    #         elif mid_value < target_value:
+    #             low = mid + 1
+    #         else:
+    #             high = mid - 1
+
+    #     return None
+    
     def __str__(self):
         return str(self.name)
     
